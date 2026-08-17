dfe1d66abe7b73fdc534ad8e4e2caa6d52045123
mattbaise <mattbaise@gmail.com>
2026-08-17 16:31:20 -0400

    Separate authentication routes into blueprint

--- forum/routes.py
+++ forum/routes.py
@@ -1,10 +1,18 @@
-from flask import render_template, request, redirect, url_for
-from flask_login import current_user, login_user, logout_user
-from flask_login.utils import login_required
 import datetime
+
 from flask import Blueprint, render_template, request, redirect, url_for
-from forum.models import User, Post, Comment, Subforum, valid_content, valid_title, db, generateLinkPath, error
-from forum.user import username_taken, email_taken, valid_username
+from flask_login import current_user, login_required
+
+from forum.models import (
+    Post,
+    Comment,
+    Subforum,
+    valid_content,
+    valid_title,
+    db,
+    generateLinkPath,
+    error,
+)
 
 ##
 # This file needs to be broken up into several, to make the project easier to work on.
@@ -12,55 +20,7 @@ from forum.user import username_taken, email_taken, valid_username
 
 rt = Blueprint('routes', __name__, template_folder='templates')
 
-@rt.route('/action_login', methods=['POST'])
-def action_login():
-	username = request.form['username']
-	password = request.form['password']
-	user = User.query.filter(User.username == username).first()
-	if user and user.check_password(password):
-		login_user(user)
-	else:
-		errors = []
-		errors.append("Username or password is incorrect!")
-		return render_template("login.html", errors=errors)
-	return redirect("/")
 
-
-@login_required
-@rt.route('/action_logout')
-def action_logout():
-	#todo
-	logout_user()
-	return redirect("/")
-
-@rt.route('/action_createaccount', methods=['POST'])
-def action_createaccount():
-	username = request.form['username']
-	password = request.form['password']
-	email = request.form['email']
-	errors = []
-	retry = False
-	if username_taken(username):
-		errors.append("Username is already taken!")
-		retry=True
-	if email_taken(email):
-		errors.append("An account already exists with this email!")
-		retry = True
-	if not valid_username(username):
-		errors.append("Username is not valid!")
-		retry = True
-	# if not valid_password(password):
-	# 	errors.append("Password is not valid!")
-	# 	retry = True
-	if retry:
-		return render_template("login.html", errors=errors)
-	user = User(email, username, password)
-	if user.username == "admin":
-		user.admin = True
-	db.session.add(user)
-	db.session.commit()
-	login_user(user)
-	return redirect("/")
 
 
 @rt.route('/subforum')
@@ -76,9 +36,7 @@ def subforum():
 	subforums = Subforum.query.filter(Subforum.parent_id == subforum_id).all()
 	return render_template("subforum.html", subforum=subforum, posts=posts, subforums=subforums, path=subforumpath)
 
-@rt.route('/loginform')
-def loginform():
-	return render_template("login.html")
+
 
 
 @login_required
